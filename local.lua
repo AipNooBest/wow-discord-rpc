a53689b26c8713b8ae6c2ca587a9d72a71ed7cec
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2021-04-12 22:45:36 +0300

    Added comment for <5.0.4

--- local.lua
+++ local.lua
@@ -3,4 +3,4 @@ playerOnBattleGround = "In battleground"
 playerIsDead = " (Dead)"
 itemLevel = " item level"
 function inGroupOfSomePeople()
-    return "In group of " .. GetNumGroupMembers() .. " people" end
+    return "In group of " .. GetNumGroupMembers() .. " people" end -- Use GetNumPartyMembers()+1 [didn't test yet for +1 thing] for <5.0.4
