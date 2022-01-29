e7bcb04c5c8c52433c9304760e0d1d24593db6c4
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-01-29 19:12:53 +0300

    Added windowed mode and pixel resizing compatibility

--- script/WoWPresence.py
+++ script/WoWPresence.py
@@ -6,27 +6,22 @@ import os
 import ast
 from PIL import Image, ImageGrab
 import win32gui
+import win32api
+import win32con
 import logging
 
 # localisation variables. Change them for your preferences.
 inMainMenu = "In main menu"
 
-# these are internal use variables, don't touch them
+# these are internal use variables, don't touch them, unless you know what you're doing.
+logging.basicConfig(filename='log.txt', filemode='w', encoding='utf-8', level=logging.INFO)
 decoded = ''
 wow_hwnd = None
 rpc_obj = None
-lastZone = None
-lastPlayerLevel = None
-lastPlayerName = None
-lastPlayerInfo = None
-lastEngClass = None
-lastPlayerState = None
-lastMapID = None
 timePlayed = None
 dir_path = os.path.dirname(os.path.realpath(__file__))
 f = open(dir_path + '/zones.txt')
 zones = ast.literal_eval(f.read())
-logging.basicConfig(filename='log.txt', filemode='w', encoding='utf-8', level=logging.INFO)
 print("The script is running!\n"
       "Now you can minimize the window and play WoW.\n"
       "The script will terminate automatically when you exit the game.\n"
@@ -40,26 +35,59 @@ def callback(hwnd, extra):
         wow_hwnd = hwnd
 
 
+def getImage(rect, offsetX, offsetY, iter):
+    new_rect = (rect[0] + offsetX, rect[1] + offsetY, rect[2], rect[1] + offsetY + 2)
+    logging.debug("Window rectangle: %s; Iteration %s", str(new_rect), iter)
+    try:
+        im = ImageGrab.grab(new_rect, all_screens=True)
+        logging.debug("Pixel at position 0,0: " + str(im.getpixel((0, 0))))
+        # Firstly try to get pixels at absolute position 0, 0 - if the game is running in borderless mode.
+        if im.getpixel((0, 0)) == (36, 36, 36):
+            return im
+        # If 0, 0 coordinates are wrong, the game is probably running in windowed mode with borders.
+        else:
+            if iter == 0:
+                # Getting height of the windows border (this may vary depending on DWM scaling, so hard-coding is bad).
+                height = (win32api.GetSystemMetrics(win32con.SM_CYCAPTION) +
+                          win32api.GetSystemMetrics(win32con.SM_CYBORDER) * 4 +
+                          win32api.GetSystemMetrics(win32con.SM_CYEDGE) * 2)
+                logging.debug("Window border height: %s" % height)
+                # Check if the game is running fullscreen-windowed mode with borders, so we apply only Y offset.
+                return getImage(rect, offsetX, height, 1)
+            elif iter == 1:
+                # The last option - the game is running in pure windowed mode, so we apply both offsets.
+                return getImage(rect, 8, offsetY, 2)
+            # If neither of the above options are true, the pixel array is probably does not exist.
+            return False
+    except Image.DecompressionBombError:
+        logging.error('DecompressionBombError')
+        return False
+
+
 def read_squares(hwnd):
     global decoded
     rect = win32gui.GetWindowRect(hwnd)
-    new_rect = (rect[0], rect[1], rect[2], rect[1]+1)
-    try:
-        im = ImageGrab.grab(new_rect)
-    except Image.DecompressionBombError:
-        logging.error('DecompressionBombError')
-        return 0
+    im = getImage(rect, 0, 0, 0)
+    if im is False:
+        return 1
 
     # Check if there's a message at the top left corner.
     # If there's none, then we're either in main menu or addon is not working.
-    r, g, b = im.getpixel((0, 0))
-    r2, g2, b2 = im.getpixel((1, 0))
-    if not r == g == b == 36 and not r2 == g2 == b2 == 0:
+    # Sometimes addon moves 1 px to the right, so we check if that is the case.
+    offset = 0
+    if im.getpixel((1, 0)) == (0, 0, 0):
+        logging.debug("Pixels are 1x1 starting at position 0")
+    # Second pixel also can be duplicated due to resizing.
+    elif im.getpixel((1, 0)) == (36, 36, 36) and (im.getpixel((2, 0)) == (0, 0, 0) or im.getpixel((2, 0)) == (36, 36, 36)):
+        logging.debug("Pixels are more than 1x1 or they start at position 1")
+        offset += 1
+    else:
+        logging.info("Could not find pixel array. You're either in main menu or addon is not working")
         return 1
 
     read = []
     duplicate = False
-    for square_idx in range(int(im.width)):
+    for square_idx in range(offset, int(im.width)):
         r, g, b = im.getpixel((square_idx, 0))
 
         if r == g == b == 255:
@@ -85,9 +113,7 @@ def read_squares(hwnd):
     parts = decoded.replace('$$$', '').split('|')
 
     # sanity check
-    if (len(parts) != 7 or
-            not decoded.endswith('$$$') or
-            not decoded.startswith('$$$')):
+    if (not decoded.endswith('$$$') or not decoded.startswith('$$$')):
         return 0
 
     return parts
@@ -113,13 +139,11 @@ def connect_to_discord():
 
 def update_activity(activity):
     global rpc_obj
-    global lastZone, lastPlayerLevel, lastPlayerName, lastPlayerInfo, lastEngClass, lastState
     try:
         rpc_obj.set_activity(activity)
     except Exception as exc:
         logging.warning('Looks like the connection to Discord was broken (%s). '
                         'I will try to connect again in 5 sec.' % str(exc))
-        lastZone, lastPlayerLevel, lastPlayerName, lastPlayerInfo, lastEngClass, lastState = None, None, None, None, None, None
         rpc_obj = None
 
 
@@ -134,7 +158,8 @@ while True:
             time.sleep(5)
             continue
 
-        elif lines == 1:  # We know that we're either in main menu or addon is not working
+        # We know that we're either in main menu or addon is not working
+        elif lines == 1:
             connect_to_discord()
             if timePlayed is None:
                 timePlayed = {'start': round(time.time())}
@@ -151,40 +176,30 @@ while True:
             continue
         else:
             zoneName, playerLevel, playerName, playerInfo, engClass, playerState, mapID = lines
+            connect_to_discord()
 
-            if zoneName != lastZone or playerState != lastPlayerState:
-                # there has been an update, so send it to discord
-                lastZone = zoneName
-                lastPlayerLevel = playerLevel
-                lastPlayerName = playerName
-                lastPlayerInfo = playerInfo
-                lastEngClass = engClass
-                lastPlayerState = playerState
-                lastMapID = mapID
-                connect_to_discord()
+            logging.info('Setting new activity: %s - %s - %s - %s - %s - %s - %s' % (
+                zoneName, playerLevel, playerName, playerInfo, engClass, playerState, mapID))
 
-                logging.info('Setting new activity: %s - %s - %s - %s - %s - %s - %s' % (
-                    zoneName, playerLevel, playerName, playerInfo, engClass, playerState, mapID))
-
-                if timePlayed is None:
-                    timePlayed = {'start': round(time.time())}
-                if mapID in zones.keys():
-                    zone = zones[str(mapID)]
-                else:
-                    zone = "wow-icon"
-                    logging.warning("The zone is not in the list: %s [ID: %s]" % (zoneName, mapID))
-                activity = {
-                    'details': "%s [%s LVL]" % (playerName, playerLevel),
-                    'state': playerState,
-                    'assets': {
-                        'large_image': zone,
-                        'large_text': zoneName,
-                        'small_image': engClass.lower(),
-                        'small_text': playerInfo
-                    },
-                    'timestamps': timePlayed
-                }
-                update_activity(activity)
+            if timePlayed is None:
+                timePlayed = {'start': round(time.time())}
+            if mapID in zones.keys():
+                zone = zones[str(mapID)]
+            else:
+                zone = "wow-icon"
+                logging.warning("The zone is not in the list: %s [ID: %s]" % (zoneName, mapID))
+            activity = {
+                'details': "%s [%s LVL]" % (playerName, playerLevel),
+                'state': playerState,
+                'assets': {
+                    'large_image': zone,
+                    'large_text': zoneName,
+                    'small_image': engClass.lower(),
+                    'small_text': playerInfo
+                },
+                'timestamps': timePlayed
+            }
+            update_activity(activity)
 
     elif not wow_hwnd and rpc_obj:
         logging.info('WoW no longer exists, terminating...')
